495cc2d9078d9cde4d1e8c630b36fb724f1a4a43
Aaron Harding <aaronh1991@gmail.com>
2025-02-14 16:01:26 +0000

    fix False for baby chair in bookings model

--- bookings/models.py
+++ bookings/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-class RestaurantBooking(models.model) : 
+class RestaurantBooking(models.Model) : 
     customer_name = models.CharField(max_length=50)
     customer_email = models.EmailField()
     customer_phone = models.CharField(max_length=15)
@@ -11,7 +11,7 @@ class RestaurantBooking(models.model) :
     booking_time = models.TimeField()
     num_guests = models.PositiveIntegerField()
     special_requests = models.TextField(blank=True, null=True)
-    baby_chair = models.BooleanField(default=false)
+    baby_chair = models.BooleanField(default=False)
 
     def __str__(self) : 
         return f"{self.customer_name} - {self.booking_date}"
