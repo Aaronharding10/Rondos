7ecf10ac575f1d7f7a2224451a0bd2896049b321
Aaron Harding <aaronh1991@gmail.com>
2025-02-14 16:33:22 +0000

    update booking model to include status field

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from.models import RestaurantBooking
+
+admin.site.register(RestaurantBooking)
 
 # Register your models here.
--- bookings/models.py
+++ bookings/models.py
@@ -4,6 +4,16 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class RestaurantBooking(models.Model) : 
+    PENDING = 'pending'
+    APPROVED = 'approved'
+    REJECTED = 'rejected'
+
+    STATUS_CHOICES = [
+        (PENDING, 'pending'),
+        (APPROVED, 'approved')
+        (REJECTED, 'rejected')
+    ]
+
     customer_name = models.CharField(max_length=50)
     customer_email = models.EmailField()
     customer_phone = models.CharField(max_length=15)
@@ -12,8 +22,12 @@ class RestaurantBooking(models.Model) :
     num_guests = models.PositiveIntegerField()
     special_requests = models.TextField(blank=True, null=True)
     baby_chair = models.BooleanField(default=False)
+    status = models.CharField(
+        max_length=10,
+        choices=STATUS_CHOICES,
+        default=PENDING,
+    )
 
     def __str__(self) : 
-        return f"{self.customer_name} - {self.booking_date}"
-    
+        return f"{self.customer_name} - {self.booking_date} - {self.status}"
 
