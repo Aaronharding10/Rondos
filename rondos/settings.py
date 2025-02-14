61c1b7a0e67d576fb09c6378c7bfb11e48be3a73
Aaron Harding <aaronh1991@gmail.com>
2025-02-14 13:50:22 +0000

    add app to allowed hosts

--- rondos/settings.py
+++ rondos/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get ('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['rondos-restaurant.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['rondos-restaurant-265904143858.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
