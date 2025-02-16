4285dc6aee749c18b828c01c2c1abd6ce70ffb06
Aaron Harding <aaronh1991@gmail.com>
2025-02-16 11:19:16 +0000

    add error handling to views

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+"""
+Error handling 404 and 500 views
+"""
+
+def custom_handler404(request, exception):
+    """
+    Custom handler for 404 (Page Not Found) errors.
+    """
+    return render(request, '404.html', status=404)
+
+
+def custom_handler500(request):
+    """
+    Custom handler for 500 (Internal Server Error) errors.
+    """
+    return render(request, '500.html', status=500)
