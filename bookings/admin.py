494b13a5d6aaeda84574d113ad437a836c3dbac9
Aaron Harding <aaronh1991@gmail.com>
2025-02-22 11:50:19 +0000

    update admin.py file

--- bookings/admin.py
+++ bookings/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from.models import RestaurantBooking
+from .models import RestaurantBooking
 
-admin.site.register(RestaurantBooking)
-
-# Register your models here.
+@admin.register(RestaurantBooking)
+class RestaurantBookingAdmin(admin.ModelAdmin):
+    list_display = ('customer_name', 'booking_date', 'booking_time', 'num_guests', 'status', 'table')
+    list_filter = ('status', 'booking_date', 'table')
+    search_fields = ('customer_name', 'customer_email')
