49606f70f0e40b8d94aba94065e3ba9e105cc9b7
Aaron Harding <aaronh1991@gmail.com>
2025-02-24 16:45:54 +0000

    add urls.py for menu

--- menu/urls.py
+++ menu/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.menu_list, name='menu_list'),
-    path('<int:id>/', views.menu_item_detail, name='menu_item_detail'),
+    path('', views.menu_list, name='menu_list'), 
 ]
