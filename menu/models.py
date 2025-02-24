28fb1d6906a6c620fab94459223d1cca3fc01ea6
Aaron Harding <aaronh1991@gmail.com>
2025-02-24 15:46:21 +0000

    add menu model

--- menu/models.py
+++ menu/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+class MenuItem(models.Model):
+    CATEGORY_CHOICES = [
+        ('main', 'Main Course'),
+        ('dessert', 'Dessert'),
+        ('drink', 'Drink'),
+    ]
+
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    category = models.CharField(max_length=10, choices=CATEGORY_CHOICES)
+    image = CloudinaryField('menu_images/', blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.name} - €{self.price}"
