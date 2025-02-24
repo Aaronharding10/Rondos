6466152d855abe2f8467057a18945969cfb3ec67
Aaron Harding <aaronh1991@gmail.com>
2025-02-24 18:24:40 +0000

    fix routing in menu views.py

--- menu/views.py
+++ menu/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-from .models import MenuItem
 
-def menu_list(request):
-    menu_items = MenuItem.objects.all() 
-    return render(request, 'menu/menu_list.html', {'menu_items': menu_items})
+def menu(request):
+    return render(request, 'menu.html') 
 
