41d1d32887dd6641a1147513d273603edbd7608b
Aaron Harding <aaronh1991@gmail.com>
2025-02-24 16:25:42 +0000

    add menu view to vierws.py

--- menu/admin.py
+++ menu/admin.py
@@ -3,4 +3,3 @@ from .models import MenuItem
 
 admin.site.register(MenuItem)
 
-# Register your models here.
--- menu/views.py
+++ menu/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import MenuItem
+
+def menu_list(request):
+    menu_items = MenuItem.objects.all()
+    return render (request, 'menu/menu_list.html', {'menu_items' : menu_items})
 
 # Create your views here.
