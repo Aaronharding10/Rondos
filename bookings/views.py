73ac9dd51c18d54b76d2a127161d2167568696aa
Aaron Harding <aaronh1991@gmail.com>
2025-02-24 16:38:56 +0000

    updated views.py for bookings

--- bookings/views.py
+++ bookings/views.py
@@ -52,22 +52,13 @@ def edit_booking(request, booking_id):
     return render(request, 'bookings/edit_booking.html', {'form': form, 'booking': booking})
 
 
-
-
-
 """
 Error handling 404 and 500 views
 """
 
 def custom_handler404(request, exception):
-    """
-    Custom handler for 404 (Page Not Found) errors.
-    """
     return render(request, '404.html', status=404)
 
 
 def custom_handler500(request):
-    """
-    Custom handler for 500 (Internal Server Error) errors.
-    """
     return render(request, '500.html', status=500)
